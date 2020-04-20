0e277f2ceb8be6176cb31ffdd261214552180ea2
Chandler Stevens <chandlerstevens22@gmail.com>
2020-04-20 09:50:20 -0700

    Added Comments to MainActivity.kt [SPEN-19]

--- Development/app/src/main/java/com/team4/walletwatch/Main/MainActivity.kt
+++ Development/app/src/main/java/com/team4/walletwatch/Main/MainActivity.kt
@@ -7,23 +7,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
+/* This is the "main" of the program and is also the primary activity of the app.
+* This will immediately load upon app launch. */
 class MainActivity : AppCompatActivity() {
     lateinit var model : SharedViewModel
 
+    /* Overwritten function that performs tasks immediately upon app launch */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        /* Display the activity_main.xml layout. */
         setContentView(R.layout.activity_main)
 
+        /* Setup the fragment manager, which will load the three tabs and select Tab 1. */
         val fragmentAdapter = MainPagerAdapter(supportFragmentManager)
         mainPager.adapter = fragmentAdapter
 
         mainTabs.setupWithViewPager(mainPager)
 
+        /* Function that will open the Settings activity when the user taps the Settings button. */
         findViewById<ImageButton>(R.id.openSettingsButton).setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
 
+        /* Setup the shared view model, so that all fragments can access the same live data. */
         val viewModelFactory = Injection.provideViewModelFactory(this)
         model = ViewModelProviders.of(this, viewModelFactory).get(
             SharedViewModel::class.java)
