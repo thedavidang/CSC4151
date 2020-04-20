d2e2c3f12928e3f9e298fa7d010a436b0eb90539
Chandler Stevens <chandlerstevens22@gmail.com>
2020-04-20 10:43:08 -0700

    Added Comments to SettingsActivity.kt [SPEN-19]

--- Development/app/src/main/java/com/team4/walletwatch/Settings/SettingsActivity.kt
+++ Development/app/src/main/java/com/team4/walletwatch/Settings/SettingsActivity.kt
@@ -7,24 +7,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_settings.*
 
+/* This is the secondary activity of the app,
+* which allows the user to view and modify various settings. */
 class SettingsActivity : AppCompatActivity() {
     lateinit var model : SharedViewModel
 
+    /* Overwritten function that performs tasks immediately upon opening Settings. */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        /* Display the activity_settings.xml layout. */
         setContentView(R.layout.activity_settings)
 
+        /* Setup the fragment manager, which will load the three tabs and select "Categories". */
         val fragmentAdapter = SettingsPagerAdapter(supportFragmentManager)
         settingsPager.adapter = fragmentAdapter
 
         settingsTabs.setupWithViewPager(settingsPager)
 
+        /* Function that will close the Settings activity when the user taps the Settings button. */
         findViewById<ImageButton>(R.id.closeSettingsButton).setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
 
+        /* Setup the shared view model, so that all fragments can access the same live data. */
         val viewModelFactory = Injection.provideViewModelFactory(this)
         model = ViewModelProviders.of(this, viewModelFactory).get(
             SharedViewModel::class.java)
