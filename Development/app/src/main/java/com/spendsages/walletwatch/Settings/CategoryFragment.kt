4ba095ee96d9a3444ba3c75cc5b095c80956949e
Chandler Stevens <chandlerstevens22@gmail.com>
2020-05-19 14:56:53 -0700

    Removed Design Folder [SPEN-59]
    Removed Design documentation.

---
+++ Development/app/src/main/java/com/spendsages/walletwatch/Settings/CategoryFragment.kt
@@ -0,0 +1,177 @@
+package com.spendsages.walletwatch
+
+import android.content.Context
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CategoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CategoryFragment : Fragment() {
+    private lateinit var rootView : View
+    private lateinit var settings : SettingsActivity
+    private lateinit var model : SharedViewModel
+
+    private lateinit var categories : MutableList<String?>
+
+    /* Array that holds the new label for each category that the user changes.
+    * If there is no change to a category, that index MUST be null. */
+    private var changed = arrayOfNulls<String?>(3)
+
+    private lateinit var saveButton : Button
+
+    private val categoryTextboxes = Array<EditText?>(3) { null }
+
+    /* Purpose: Controller method that disables and greys-out Save Changes button or
+    * enables and reveals the Save Changes button.
+    *
+    * Parameters: enable represents a Boolean of whether or not
+    *             to enable the Save Changes button.
+    *
+    * Returns: Nothing. */
+    private fun toggleSaveButton(enable : Boolean) {
+        /* Only enable if not already enabled. */
+        if (enable && !saveButton.isEnabled) {
+            saveButton.isEnabled = true
+            saveButton.isClickable = true
+            /* Set opacity to 100 % */
+            saveButton.alpha = 1.0F
+        }
+        /* Only disable if not already disabled. */
+        else if (!enable && saveButton.isEnabled) {
+            saveButton.isEnabled = false
+            saveButton.isClickable = false
+            /* Set opacity to 50 % */
+            saveButton.alpha = 0.5F
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        rootView = inflater.inflate(R.layout.fragment_category, container, false)
+        settings = activity as SettingsActivity
+        model = settings.model
+
+        /* Grab the labels of the categories as they currently are in the XML file. */
+        categories = DataManager.getCategories(model.get())
+
+        saveButton = rootView.findViewById(R.id.saveButton)
+        /* Disable and grey-out the Save Changes button,
+        * since the user has not made any changes yet. */
+        toggleSaveButton(false)
+
+        /* This listener for the Save Change button simply opens the Confirmation alert. */
+        saveButton.setOnClickListener {
+            /* TODO (SPEN-37): Simply open the Confirmation alert inside this listener,
+            *   which likely will be the only line in this function. */
+
+            /* TODO (SPEN-11): Move below code out of this listener and into the code for
+            *   the Confirmation alert. This, of course, first requires creating the UI widget for
+            *   the Confirmation alert first and writing the setup code for it. */
+            DataManager.changeCategories(settings, model.get(), changed)
+            model.save(settings)
+            toggleSaveButton(false)
+        }
+
+        /* Populate the array of category textboxes. */
+        categoryTextboxes[0] = rootView.findViewById(R.id.category1Edit)
+        categoryTextboxes[1] = rootView.findViewById(R.id.category2Edit)
+        categoryTextboxes[2] = rootView.findViewById(R.id.category3Edit)
+
+        /* Iterate through each category textbox. */
+        for ((index, textbox) in categoryTextboxes.withIndex()) {
+            /* Set the label for each category textbox as they currently are in the XML file. */
+            textbox!!.setText(categories[index + 1])
+
+            /* Listener that checks if the user changed the category textbox content. */
+            textbox.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence, start: Int, count: Int, after: Int) {}
+
+                /* TODO (SPEN-37): Modify this listener such that the Save Changes button
+                *   is enabled if and only if all three category textboxes actually have
+                *   word(s) in them AND at least one of them has a category label that is
+                *   not in the "categories" array.
+                *   In addition, make sure to update the "changed" array accordingly,
+                *   such that new category labels are placed in the correct position in the
+                *   "changed" array, but the position of an existing category label is "null". */
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    /* Remove all whitespace from user input in category textbox. */
+                    val trimmed = s.toString().trim { it <= ' ' }
+
+                    /* Check if the user did not enter any word(s) into the category textbox. */
+                    if (trimmed.isEmpty()) {
+                        /* Disable and grey-out the Save Changes button. */
+                        toggleSaveButton(false)
+
+                        /* TODO (SPEN-37): Move/modify this line as necessary.
+                        *   This simply exists currently to serve as a demonstrable way
+                        *   to show that changing categories works, but you will
+                        *   need to implement additional code that correctly sets up
+                        *   the "changed" array as mentioned above.*/
+                        changed[index] = null
+                    }
+                    else {
+                        /* Enable and reveal the Save Changes button. */
+                        toggleSaveButton(true)
+
+                        /* TODO (SPEN-37): Move/modify this line as necessary.
+                        *   This simply exists currently to serve as a demonstrable way
+                        *   to show that changing categories works, but you will
+                        *   need to implement additional code that correctly sets up
+                        *   the "changed" array as mentioned above.*/
+                        changed[index] = categoryTextboxes[index]!!.text.toString()
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable) {}
+            })
+        }
+
+        rootView.setOnTouchListener { _: View, _: MotionEvent ->
+            /* Hide the keyboard. */
+            (settings.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .hideSoftInputFromWindow(categoryTextboxes[0]!!.windowToken, 0)
+        }
+
+        return rootView
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CategoryFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CategoryFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ Development/app/src/main/java/com/spendsages/walletwatch/SharedViewModel.kt
@@ -0,0 +1,102 @@
+package com.spendsages.walletwatch
+
+import android.app.Activity
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.w3c.dom.Document
+import java.io.OutputStreamWriter
+import java.io.StringWriter
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.Transformer
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+
+/* This class provides support for sharing live data amongst different views. */
+class SharedViewModel() : ViewModel() {
+    private val doc = MutableLiveData<Document>()
+
+    /* Purpose: Opens the local repo XML file and pipes the contents into doc.
+    *
+    * Parameters: activity represents the app Activity context that needs the shared view model.
+    *
+    * Returns: Nothing. */
+    fun open(activity: Activity) {
+        /* Locate XML file in Android Internal Storage */
+        var file = activity.getFileStreamPath(activity.getString(R.string.docFilenameString))
+
+        /* Check if XML file does not yet exist since
+        * app has likely just now been installed on user's device. */
+        if(!file.exists()) {
+            try {
+                /* Open skeleton XML file from assets. */
+                val outputWriter = OutputStreamWriter(
+                    activity.openFileOutput(activity.getString(R.string.docFilenameString), Context.MODE_PRIVATE))
+
+                /* Write the contents of the skeleton XML into buffer. */
+                outputWriter.write(activity.assets.open(
+                    activity.getString(R.string.docFilenameString)).bufferedReader().use{it.readText()})
+
+                /* Close the buffer. */
+                outputWriter.close()
+
+                /* Open the newly created XML file in Android Internal Storage. */
+                file = activity.getFileStreamPath(activity.getString(R.string.docFilenameString))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        /* Load the contents of the XML file to the value of doc. */
+        doc.value = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file)
+    }
+
+    /* Purpose: Getter/Accessor that retrieves the contents of the XML file.
+    *
+    * Parameters: None.
+    *
+    * Returns: Contents of the XML file as they exist in the value of doc. */
+    fun get() : Document {
+        return doc.value!!
+    }
+
+    /* Purpose: Converts the contents of the value of doc into a string.
+    *
+    * Parameters: doc represents the Document of the local repo XML file.
+    *
+    * Returns: String of the contents of the value of doc. */
+    private fun docString(doc : Document?) : String {
+        val tf: TransformerFactory = TransformerFactory.newInstance()
+        val trans: Transformer = tf.newTransformer()
+        val sw = StringWriter()
+
+        trans.transform(DOMSource(doc), StreamResult(sw))
+
+        return sw.toString()
+    }
+
+    /* Purpose: Completely overwrites the contents of the local repo XML file
+    * with the string contents of the value of doc.
+    *
+    * Parameters: activity represents the app Activity context that needs the shared view model.
+    *
+    * Returns: Nothing. */
+    fun save(activity: Activity) {
+        try {
+            /* Open the local repo XML file in MODE_PRIVATE (Forced Overwrite) mode. */
+            val outputWriter = OutputStreamWriter(
+                activity.openFileOutput(activity.getString(R.string.docFilenameString), Context.MODE_PRIVATE))
+
+            /* Convert the value of doc into a string and
+            * write it to the now empty local repo XML file */
+            outputWriter.write(docString(doc.value))
+
+            /* Close the buffer. */
+            outputWriter.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
