ad8aa73267f16f212f4ee802c123b6b7611c6372
Jason D <45178868+jasond299@users.noreply.github.com>
2020-05-26 18:10:27 -0700

    Add files via upload

--- Development/app/src/main/java/com/spendsages/walletwatch/Main/Tab3Fragment.kt
+++ Development/app/src/main/java/com/spendsages/walletwatch/Main/Tab3Fragment.kt
@@ -219,8 +219,10 @@ class Tab3Fragment : Fragment() {
     * Returns: Nothing. */
     private fun submitEdit() {
         /* Add the entry to the local repo. */
+        val date = modelDateFormat.format(userDateFormat.parse(changedInputs[2]!!)!!)
+        val category = (changedInputs[3]!!.toInt() + 1).toString()
         DataManager.editEntry(model.get(), entryID,
-            changedInputs[0]!!, changedInputs[1]!!, changedInputs[2]!!, changedInputs[3]!!)
+            changedInputs[0]!!, changedInputs[1]!!, date, category)
 
         /* Update the data model. */
         model.save(main)
