24ffe9e9d7aedea9ccefa5864c6e6cc93ba38b96
David <angd@spu.edu>
2020-05-27 20:49:41 -0700

    Cleared the selectedEntries array [SPEN-32]

--- Development/app/src/main/java/com/spendsages/walletwatch/DataManager.kt
+++ Development/app/src/main/java/com/spendsages/walletwatch/DataManager.kt
@@ -777,8 +777,11 @@ object DataManager {
         // Iterate through the entries selected
         for(selectedEntries in selectedEntries)
         {
+            //delete the entry
             deleteEmptyTags(doc,selectedEntries)
         }
+        //Clear the array so it is empty
+        selectedEntries.clear()
     }
 
     /* Purpose: Edit an entry in the local repo XML file.
