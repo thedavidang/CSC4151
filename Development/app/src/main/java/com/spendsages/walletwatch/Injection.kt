aeee34f98da0342d9b1bf921f0627d41b58f498f
Chandler Stevens <chandlerstevens22@gmail.com>
2024-03-05 22:13:52 -0800

    Suppress Unused Parameter Warning
    The context parameter is not used, but still is good to have to maintain compatibility.

--- Development/app/src/main/java/com/spendsages/walletwatch/Injection.kt
+++ Development/app/src/main/java/com/spendsages/walletwatch/Injection.kt
@@ -5,7 +5,7 @@ import android.content.Context
 /* This class provides support for injecting a shared view model into
 * an app activity, which allows live data updating. */
 object Injection {
-    fun provideViewModelFactory(context: Context): ViewModelFactory {
+    fun provideViewModelFactory(@Suppress("UNUSED_PARAMETER")context: Context): ViewModelFactory {
         return ViewModelFactory()
     }
 }
